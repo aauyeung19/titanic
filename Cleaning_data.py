67b29a6a5892a8b5b84cdd40406ee2a23be941ee
aauyeung19 <59631975+aauyeung19@users.noreply.github.com>
2020-08-25 16:41:50 -0400

    Uploaded Local Files

---
+++ Cleaning_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 25 16:20:37 2020
+
+@author: andrew
+"""
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('train.csv')
